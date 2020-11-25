b2b92f9b8026d6f827fff85605e449d322762d46
DavideTB <davide.tornielli@mail.polimi.it>
2020-11-25 23:54:03 +0100

    Bozza pagina principale

--- flutter_app/lib/src/profile.dart
+++ flutter_app/lib/src/profile.dart
@@ -26,7 +26,7 @@ class _ProfileState extends State<Profile> {
       widget.profile = myProfile;
       if (getLoggedF()){
         name = myProfile["name"];
-        //photo = myProfile["picture"]["data"]["url"];
+        photo = myProfile["picture"]["data"]["url"];
         email = myProfile["email"];
       }
       if (getLoggedG()){
@@ -49,23 +49,103 @@ class _ProfileState extends State<Profile> {
 
     return MaterialApp(
       home: Scaffold(
-        body: Center(
-            child: Column (
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Text ("$name"),
-                //Image.network(photo, height: 150.0, width: 150.0,),
-                Text ("$email"),
-                OutlineButton(
-                  child: Text("See map"),
-                  onPressed: () {
-                    Navigator.push(context, MaterialPageRoute(builder:(context)=> Map()));
-                    },
+
+        appBar: AppBar(
+
+          title: Row(
+            children: [
+              Icon(
+                Icons.menu,
+                color: Colors.grey[150],
+              ),
+              SizedBox(),
+              Text(
+                  'Welcome back',
+                style: TextStyle(
+                    color: Colors.grey[150],
+                    letterSpacing: 1.5,
+                    fontFamily: 'FredokaOne',
                 ),
-              ]
-            )
+              ),
+            ],
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.amber[400],
         ),
+
+          body: Column (
+            children: [
+              Row(
+                children: [
+                  Image.network(photo, height: 150.0, width: 150.0,),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+
+                      SizedBox(height: 0.0),
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.account_box_outlined,
+                            color: Colors.grey[600],
+                          ),
+                          SizedBox(width: 10.0,),
+                          Text(
+                            "$name",
+                            style: TextStyle(
+                              color: Colors.grey[600],
+                              fontSize: 15.0,
+                              letterSpacing: 1.0,
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 10.0),
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.email,
+                            color: Colors.grey[600],
+                          ),
+                          SizedBox(width: 10.0,),
+                          Text(
+                            "$email",
+                            style: TextStyle(
+                              color: Colors.grey[600],
+                              fontSize: 15.0,
+                              letterSpacing: 1.0,
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  )
+                ],
+                //mainAxisAlignment: MainAxisAlignment.end,
+              ),
+              OutlineButton(
+                child: Text("See map"),
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder:(context)=> Map()));
+                },
+              ),
+            ]
+
+              // crossAxisAlignment: CrossAxisAlignment.start,
+              // mainAxisSize: MainAxisSize.min,
+              // children: [
+              //   Text ("$name"),
+              //   Image.network(photo, height: 150.0, width: 150.0,),
+              //   Text ("$email"),
+              //   OutlineButton(
+              //     child: Text("See map"),
+              //     onPressed: () {
+              //       Navigator.push(context, MaterialPageRoute(builder:(context)=> Map()));
+              //       },
+              //   ),
+
+            )
+
       ),
     );
   }
