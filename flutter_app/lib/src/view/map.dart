eefe3e349160872a457022861bf7a0ed45a8dadd
DavideTB <davide.tornielli@mail.polimi.it>
2021-01-07 18:49:47 +0100

    searchBar map

--- flutter_app/lib/src/view/map.dart
+++ flutter_app/lib/src/view/map.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/src/view/userAccountWidget.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:intl/intl.dart';
 import '../services/service.dart';
 import 'package:location/location.dart';
 import 'displayMenuStation.dart';
-
-
-
+import '../services/stationServices.dart';
 
 class Map extends StatefulWidget {
   @override
@@ -16,28 +15,34 @@ class Map extends StatefulWidget {
 class _MapState extends State<Map> {
   Set<Marker> _markers;
   GoogleMapController mapController;
+  final TextEditingController _controller= TextEditingController();
+  List stations = [];
   final LatLng _center = const LatLng(45.456532, 9.125001);
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
   }
 
-  void setMarkers () async{
+  void setMarkers() async {
     var response = await retrieveMarkers();
     setState(() {
       _markers = buildMarkers(response, context);
     });
-
   }
 
   void _getLocationPermission() async {
     var location = new Location();
     try {
-
       location.requestPermission();
     } on Exception catch (_) {
       print('There was a problem allowing location access');
     }
+  }
+
+  void updateStations(String search) {
+    searchStationByName(search).then((netStations) => setState(() {
+          stations = netStations;
+        }));
   }
 
   void initState() {
@@ -48,26 +53,149 @@ class _MapState extends State<Map> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Map'),
-          backgroundColor: Colors.green[700],
-        ),
-        drawer: UserAccount(),
-        body: GoogleMap(
-          onMapCreated: _onMapCreated,
-          initialCameraPosition: CameraPosition(
-            target: _center,
-            zoom: 11.0,
-          ),
-          markers: _markers,
-          myLocationEnabled: true,
-          myLocationButtonEnabled: true,
+    var _blankFocusNode = new FocusNode();
 
-        ),
+
+    return Scaffold(
+      resizeToAvoidBottomPadding: false,
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        title: Text('Map'),
+        backgroundColor: Colors.green[700],
       ),
-    );
+      drawer: UserAccount(),
+      body: Stack(children: [
+
+            GoogleMap(
+              onMapCreated: _onMapCreated,
+              initialCameraPosition: CameraPosition(
+                target: _center,
+                zoom: 12.0,
+              ),
+              markers: _markers,
+              myLocationEnabled: true,
+              myLocationButtonEnabled: true,
+            ),
+
+            Positioned(
+              top: 10.0,
+              left: 15.0,
+              right: 15.0,
+              child: Container(
+                padding: EdgeInsets.all(5),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(
+                    color: Colors.blue[900],
+                    width: 2.0,
+                  ),
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                child: Column(
+                  children: [
+                    Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Flexible(
+                            child: TextField(
+                              controller: _controller,
+                              textAlign: TextAlign.start,
+                              style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w500,
+                                  color: Colors.black),
+                              decoration: InputDecoration(
+                                contentPadding: EdgeInsets.symmetric(vertical: 5.0, horizontal: 10),
+                                isDense: true,
+                                fillColor: Colors.white,
+                                filled: true,
+                                hintText: 'Search Station',
+                                hintStyle: TextStyle(color: Colors.grey, fontSize: 18),
+                                enabledBorder:
+                                // UnderlineInputBorder(
+                                //   borderSide: BorderSide(
+                                //     color: Colors.blue[900],
+                                //     width: 2.0,
+                                //   ),
+                                //   //borderRadius: BorderRadius.circular(10.0),
+                                // ),
+                                InputBorder.none,
+                                focusedBorder:
+                                // UnderlineInputBorder(
+                                //   borderSide: BorderSide(
+                                //     color: Colors.blue[900],
+                                //     width: 2.0,
+                                //   ),
+                                //   //borderRadius: BorderRadius.circular(10.0),
+                                // ),
+                                InputBorder.none,
+                              ),
+                              onChanged: updateStations,
+                            ),
+                          ),
+                          IconButton(
+                            icon: Icon(
+                              Icons.clear,
+                              size: 16.0,
+                            ),
+                            padding: EdgeInsets.zero,
+                            constraints: BoxConstraints(),
+                            onPressed: () {
+                              _controller.clear();
+                              updateStations('null');
+                              },
+                          ),
+                        ],
+                      ),
+
+
+                    ConstrainedBox(
+                      constraints: BoxConstraints(
+                        maxHeight: 200,
+                      ),
+                      child: ListView.separated(
+                        shrinkWrap: true,
+                        itemCount: stations.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          return ListTile(
+                            tileColor: Colors.white,
+                            contentPadding:
+                                EdgeInsets.symmetric(horizontal: 5.0),
+                            leading: Container(
+                              child: Image(
+                                image: AssetImage("assets/M1.png"),
+                                height: 30.0,
+                                width: 45.0,
+                              ),
+                            ),
+                            title: Text(stations[index]["name"],
+                                style: TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.w500,
+                                    color: Colors.black)),
+                            onTap: () => {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => MenuStation(
+                                          name: stations[index]['name'])))
+                            },
+                            dense: true,
+                          );
+                        },
+                        separatorBuilder: (BuildContext context, int index) {
+                          return Divider(color: Colors.grey, thickness: 1);
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ]));
   }
 }
 
+// Widget searchBar(){
+//   return
+// }
